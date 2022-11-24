08bf6a8e0f6ffed67e0057ae13235fa3956abdd3
k1 <kishoorjaipal477+k1@gmail.com>
2022-11-24 07:03:59 +0000

    kuch

--- src/components/HandlewhetherData.js
+++ src/components/HandlewhetherData.js
@@ -1,28 +1,29 @@
 import React from 'react'
 
 const HandlewhetherData = (props) => {
-    const  {whetherData} = props
+    const  {whetherData , city} = props
     console.log("whetherData", whetherData)
+    console.log("city", city)
    return (
-    <div>
-
-
+    <div className=''>
+<div className='flex items-center'>
+<h1 className='p-5'>Current Codition of <span className='font-bold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-violet-500 mt-1 '> {whetherData.region} </span></h1>
+</div>
 <section className="text-gray-600 body-font">
-    <div className="container px-5 py-24 mx-auto flex flex-wrap">
+    <div className="container px-5  mx-auto flex flex-wrap">
       <div className="flex flex-wrap -m-4">
         <div className="p-4   md:w-full">
-          <div className="flex border-2 rounded-lg border-gray-200  p-8 sm:flex-row flex-col">
+          <div className="flex rounded-lg p-8 sm:flex-row flex-col">
             <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 flex-shrink-0">
               
-            <img src='https://ssl.gstatic.com/onebox/weather/64/fog.png'></img>
+            <img src={`${whetherData.currentConditions.iconURL}`}></img>
             </div>
             <div className="flex-grow">
               <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
-                Shooting Stars
+                 {whetherData.currentConditions.dayhour}
               </h2>
               <p className="leading-relaxed text-base">
-                Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-                taxidermy. Gastropub indxgo juice poutine. Wikipedia is een online encyclopedie die ernaar streeft informatie te bieden in alle erkende talen ter wereld, die vrij herbruikbaar, objectief en ...Wikipedia is een online encyclopedie die ernaar streeft informatie te bieden in alle erkende talen ter wereld, die vrij herbruikbaar, objectief en ...Wikipedia is een online encyclopedie die ernaar streeft informatie te bieden in alle erkende talen ter wereld, die vrij herbruikbaar, objectief en ...Wikipedia is een online encyclopedie die ernaar streeft informatie te bieden in alle erkende talen ter wereld, die vrij herbruikbaar, objectief en ...Wikipedia is een online encyclopedie die ernaar streeft informatie te bieden in alle erkende talen ter wereld, die vrij herbruikbaar, objectief en ...
+                 <div> Tempreture {whetherData.currentConditions.temp.c} celsius , {whetherData.currentConditions.temp.f}</div>
               </p>
               <a className="mt-3 text-blue-500 inline-flex items-center">
                 Learn More
--- src/components/home.js
+++ src/components/home.js
@@ -20,7 +20,7 @@ setFormdata({city:''})
 
  }
   return (
-    <div className='container'>
+    <div className='container px-8'>
          <form> 
       <div className="mt-3 flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
   <div className="relative flex-grow w-full">
@@ -30,7 +30,7 @@ setFormdata({city:''})
     </label>
     <input
       type="text"
-      id="full-name"
+      id="full-name" 
       placeholder='Enter city name'
       name="city"
       value={formdata.city}
@@ -40,13 +40,13 @@ setFormdata({city:''})
    
   </div>
   
-  <button onClick={handleformSubmit} className="text-white bg-blue-500 border-0 py-2 px-4 md:px-8 focus:outline-none hover:bg-blue-600 rounded md:text-lg">
+  <button onClick={handleformSubmit} className="text-white bg-blue-500 border-0 py-2 px-4 md:px-8 focus:outline-none hover:bg-blue-600 rounded md:text-lg bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
     Search
   </button>
   
 </div>
 </form>
-{cond? <HandlewhetherData  whetherData = {whetherData}/>:<h1 className='ml-8'>Enter a city name to get the whether  information of that city</h1>
+{cond? <HandlewhetherData  whetherData = {whetherData} city ={formdata && formdata.city}/>:<div className='flex  justify-center h-screen'> <h1 className='  mt-7 p-5 '>Enter a city name to get the whether  information of that city</h1></div>
    }
   
   </div>
