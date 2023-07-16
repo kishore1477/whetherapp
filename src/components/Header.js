b682e61d7dc49638b6d105b79c8f8818fea505a7
Kishore Kumar <92790475+kishore1477@users.noreply.github.com>
2023-07-16 08:48:06 +0000

    add source code link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,19 @@ const Header = () => {
   return (
     <header className="text-gray-600 body-font  md:shadow-md">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 mr-4">
       <img src='https://cdn.jim-nielsen.com/ios/512/weather-2019-02-07.png' className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"></img>
        
         <span className="ml-3 text-xl">Whether App</span>
       </a>
+      |
+      <span className='font-bold text-blue-500 ml-10' >
+        <a href='https://github.com/kishore1477/whetherapp' target="_blank">
+        Source code
+        </a>
+      </span>
       <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        
+       
       </nav>
       
     </div>
